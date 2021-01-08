823e7b8cfb1386f599b0d20f2287c1d978a20b1b
Florent <pelletier.ft1@gmail.com>
2021-01-08 09:35:16 +0100

    clean

--- resources/views/layouts/page_templates/guest.blade.php
+++
@@ -1,9 +0,0 @@
-@include('layouts.navbars.navs.guest')
-<div class="wrapper wrapper-full-page">
-   <div class=" page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('material') }}/img/login.jpg')"; background-size: cover; background-position: top center;align-items: center;" data-color="purple">
-<!-- page-header -->
-
-   @yield('content')
-    @include('layouts.footers.guest')
-  </div>
-</div>
